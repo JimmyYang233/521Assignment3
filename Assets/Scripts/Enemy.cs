f374b7ff6cd742494d0f21b20f843ec3038eae9e
Dan Ning Yang <ydn2012@hotmail.com>
2018-11-15 13:38:38 -0500

    4th push

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+	public int speed;
+
+	private CharacterController charController;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		charController = this.GetComponent<CharacterController>();
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		move();
+	}
+
+	void move()
+	{
+		charController.SimpleMove(transform.forward * speed);
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		Debug.Log("Was here");
+	}
+}
